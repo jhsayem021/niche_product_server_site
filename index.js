b3113d670ba791f23940668f8fb0e6a107df3e19
nothing021 <92251653+nothing021@users.noreply.github.com>
2021-11-19 22:45:40 +0600

    hello

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ admin.initializeApp({
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qnnq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qnnq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
